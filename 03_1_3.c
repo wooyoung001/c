f66862c3104c8930c4b97c2f8848a599c904c2cb
koreaNeymar11 <dadum123@naver.com>
2025-08-18 12:19:25 +0900

    03_1_3

---
+++ 03_1_3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+    int num,result;
+    printf("하나의 정수를 입력하시오");
+    scanf("%d",&num);
+    result=num*num;
+    printf("%d",result);
+    return 0;
+    
+}
