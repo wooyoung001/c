1536afd3ea2d2f6545256954710152caf94cd4b1
koreaNeymar11 <dadum123@naver.com>
2025-08-22 14:33:44 +0900

    nineninedan

---
+++ nineninedan.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void)
+{
+    int dan=0,num=1;
+    printf("¸î ´Ü");
+    scanf("%d",&dan);
+
+    while(num<10)
+    {
+        printf("%d*%d=%d\n",dan,num,dan*num);
+        num++;
+    }
+    return 0;
+
+}
