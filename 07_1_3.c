0c7b3ff755540d613743384e1104ea024f18d37d
koreaNeymar11 <dadum123@naver.com>
2025-08-22 17:53:36 +0900

    07_1_3

--- 07_1_3.c
+++ 07_1_3.c
@@ -1,10 +1,26 @@
 #include<stdio.h>
-int main(void)
+/*int main(void)
 {
     int num=1,sum=0;
-    while(num)
+    while(num!=)
     {
-        printf("������ �Է��Ͻÿ�");
+        printf("정수를 입력하시오");
+        scanf("%d",&num);
+        sum=sum+num;
+    }
+    printf("%d",sum);
+    return 0;
+}*/
+
+int main(void)
+{
+    int num,sum=0;
+    printf("정수를 입력하시오");
+    scanf("%d",&num);
+    sum=sum+num;
+    while(num!=0)
+    {
+        printf("정수를 입력하시오");
         scanf("%d",&num);
         sum=sum+num;
     }
