70c219ba465e6ad485024e87b60ebcc95c805940
koreaNeymar11 <dadum123@naver.com>
2025-08-22 16:33:38 +0900

    twotonine.c

---
+++ twotonine.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(void)
+{
+    int cur=2;
+    int is=0;
+    while(cur<10)
+    {
+        is=1;
+        while(is<10)
+        {
+            printf("%d*%d=%d",cur,is,cur*is);
+            is++;
+        }
+        cur++;
+
+    }
+    return 0;
+}
