4463c00ad3968463b94bcd197b403bebe937abce
koreaNeymar11 <dadum123@naver.com>
2025-08-25 14:52:58 +0900

    07_3_3

---
+++ 07_3_3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(void)
+{
+    int cur=2;
+    int is=0;
+    do
+    {
+        is=1;
+        do
+        {
+            printf("%d*%d=%d\n",cur,is,cur*is);
+            is++;
+
+        }while(is<10);
+        cur++;
+        
+    }while(cur<10);
+    return 0;
+    
+    
+}
